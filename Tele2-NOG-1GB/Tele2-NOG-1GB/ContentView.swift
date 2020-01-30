49c6b356e76f7995426ac6b971a1aa8d5cd27c84
Erik Waterham <waterham@gmail.com>
2020-01-30 16:50:38 +0100

    first commit, with SwiftLint warnings

---
+++ Tele2-NOG-1GB/Tele2-NOG-1GB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tele2-NOG-1GB
+//
+//  Created by Erik Waterham on 30/01/2020.
+//  Copyright © 2020 Erik Waterham. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tele2-NOG-1GB/Tele2-NOG-1GBTests/Tele2_NOG_1GBTests.swift
@@ -0,0 +1,34 @@
+//
+//  Tele2_NOG_1GBTests.swift
+//  Tele2-NOG-1GBTests
+//
+//  Created by Erik Waterham on 30/01/2020.
+//  Copyright © 2020 Erik Waterham. All rights reserved.
+//
+
+import XCTest
+@testable import Tele2_NOG_1GB
+
+class Tele2_NOG_1GBTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
